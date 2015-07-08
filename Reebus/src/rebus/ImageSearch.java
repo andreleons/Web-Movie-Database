f9361233c2188d0b59bab32947d745f1fb5981ef
MarshallStreeter <marshallstreeter@gmail.com>
2015-07-07 21:11:14 -0500

    Add Utf-8 encoding to telugu in url for image search

--- Reebus/src/rebus/ImageSearch.java
+++ Reebus/src/rebus/ImageSearch.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
@@ -21,7 +22,8 @@ public class ImageSearch {
 	public String getImageUrl(){
 		 String imageUrl="";
 		  try{
-	            URL url = new URL("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+word);
+			  
+	            URL url = new URL("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+URLEncoder.encode(word, "UTF-8"));
 	            URLConnection connection = url.openConnection();
 	            String line;
 	            StringBuilder builder = new StringBuilder();
