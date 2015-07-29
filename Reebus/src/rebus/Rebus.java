093be356c4a25b1bff39fee627fe6ebe645a6414
MarshallStreeter <marshallstreeter@gmail.com>
2015-07-28 19:15:05 -0500

    Rebus N Processing added

--- Reebus/src/rebus/Rebus.java
+++ Reebus/src/rebus/Rebus.java
@@ -6,66 +6,74 @@ import java.util.Collections;
 import java.util.Random;
 
 public class Rebus {
-	
+
 	Rebus() {
 		Config.entireCollection = new BigWordCollection();
-		//Set Config.gameCollection to the appropriate settings
+		// Set Config.gameCollection to the appropriate settings
 	}
-	
-	//Sets Config.gameCollection to the appropriate settings
+
+	// Sets Config.gameCollection to the appropriate settings
 	public void generateWordBank() {
-		BigWordCollection temp = Config.entireCollection.getBigWordCollectionByTopic(Config.wordBankTopic);
+		BigWordCollection temp = Config.entireCollection
+				.getBigWordCollectionByTopic(Config.wordBankTopic);
 		BigWordCollection teluguSolutions = new BigWordCollection();
 		teluguSolutions.getAllBigWords().clear();
 		WordProcessor word;
-		//0 Represents any solution length
+		// 0 Represents any solution length
 		if (Config.wordBankMaxLength != 0 && Config.LANGUAGE.equals("En")) {
-			temp = temp.getBigWordCollectionByWordLength(0, Config.wordBankMaxLength);
-			
-		} else if (Config.wordBankMaxLength != 0 ){
-			for(BigWord bigWord: temp.getAllBigWords()){
+			temp = temp.getBigWordCollectionByWordLength(0,
+					Config.wordBankMaxLength);
+
+		} else if (Config.wordBankMaxLength != 0) {
+			for (BigWord bigWord : temp.getAllBigWords()) {
 				word = new WordProcessor(bigWord.getTelugu());
-				if(word.getLogicalChars().size() <= Config.wordBankMaxLength){
+				if (word.getLogicalChars().size() <= Config.wordBankMaxLength) {
 					teluguSolutions.addBigWord(bigWord);
-					//System.out.println("Logical Size during selection: "+word.getLogicalLength());
+					// System.out.println("Logical Size during selection: "+word.getLogicalLength());
 				}
 			}
-			System.out.println("Telugu List Size: "+teluguSolutions.size());
+			System.out.println("Telugu List Size: " + teluguSolutions.size());
 			temp = teluguSolutions;
 		}
-		temp = temp.getBigWordCollectionByWordStrength(Config.wordBankWordStrengthMin, Config.wordBankWordStrengthMax);
+		temp = temp.getBigWordCollectionByWordStrength(
+				Config.wordBankWordStrengthMin, Config.wordBankWordStrengthMax);
 		Config.gameCollectionWordBank = temp;
 	}
-	
+
 	public void generateSolutionBank() {
-		BigWordCollection temp = Config.entireCollection.getBigWordCollectionByTopic(Config.solutionBankTopic);
+		BigWordCollection temp = Config.entireCollection
+				.getBigWordCollectionByTopic(Config.solutionBankTopic);
 		BigWordCollection teluguSolutions = new BigWordCollection();
 		teluguSolutions.getAllBigWords().clear();
 		WordProcessor word;
-		//0 Represents any solution length
+		// 0 Represents any solution length
 		if (Config.solutionLength != 0 && Config.LANGUAGE.equals("En")) {
 			temp = temp.getBigWordCollectionByWordLength(Config.solutionLength);
-			
-		} else if (Config.solutionLength != 0 ){
-			for(BigWord bigWord: temp.getAllBigWords()){
+
+		} else if (Config.solutionLength != 0) {
+			for (BigWord bigWord : temp.getAllBigWords()) {
 				word = new WordProcessor(bigWord.getTelugu());
-				if(word.getLogicalChars().size() == Config.solutionLength){
+				if (word.getLogicalChars().size() == Config.solutionLength) {
 					teluguSolutions.addBigWord(bigWord);
-					//System.out.println("Logical Size during selection: "+word.getLogicalLength());
+					// System.out.println("Logical Size during selection: "+word.getLogicalLength());
 				}
 			}
-			System.out.println("Telugu List Size: "+teluguSolutions.size());
+			System.out.println("Telugu List Size: " + teluguSolutions.size());
 			temp = teluguSolutions;
 		}
-		temp = temp.getBigWordCollectionByWordStrength(Config.solutionBankWordStrengthMin, Config.solutionBankWordStrengthMax);
+		temp = temp.getBigWordCollectionByWordStrength(
+				Config.solutionBankWordStrengthMin,
+				Config.solutionBankWordStrengthMax);
 		Config.gameCollectionSolutionBank = temp;
 	}
-	
-	public void findGameWords(){
+
+	public void findGameWords() {
 		Config.gameBigWords.clear();
 		BigWordCollection possibleWords = Config.gameCollectionWordBank;
 
-		BigWordCollection trimmedWords = possibleWords.getBigWordCollectionByWordLength(Config.rebusX, Config.MAX_WORD_LENGTH);
+		BigWordCollection trimmedWords = possibleWords
+				.getBigWordCollectionByWordLength(Config.rebusX,
+						Config.MAX_WORD_LENGTH);
 		ArrayList<BigWord> bigWords = trimmedWords.getAllBigWords();
 		ArrayList<ArrayList<String>> readableWords = new ArrayList<ArrayList<String>>();
 		ArrayList<Integer> randomIndexes = new ArrayList<Integer>();
@@ -73,9 +81,9 @@ public class Rebus {
 			randomIndexes.add(i);
 		}
 		WordProcessor word;
-		//generate all the parsed words
-		for(BigWord bigWord: bigWords){
-			if(Config.LANGUAGE.equals("En")){
+		// generate all the parsed words
+		for (BigWord bigWord : bigWords) {
+			if (Config.LANGUAGE.equals("En")) {
 				word = new WordProcessor(bigWord.getEnglish());
 				readableWords.add(word.getLogicalChars());
 			} else {
@@ -85,61 +93,71 @@ public class Rebus {
 		}
 
 		ArrayList<String> urls = new ArrayList<String>();
-		for(int i = 0; i< Config.solutionWord.size();i++){
-			//create some randomness for different boards to be made from same solution word
+		for (int i = 0; i < Config.solutionWord.size(); i++) {
+			// create some randomness for different boards to be made from same
+			// solution word
 			Collections.shuffle(randomIndexes);
 
 			ArrayList<String> temp = new ArrayList<String>();
 			for (int j = 0; j < readableWords.size(); j++) {
 
+				int index = 0;
 				temp = readableWords.get(randomIndexes.get(j));
-				if(temp.get(Config.rebusX-1).equals(Config.solutionWord.get(i)))
-				{
+				if (Config.rebusX == 6) {
+					index = new Random().nextInt(temp.size()-1);
+				} else {
+					index = Config.rebusX-1;
+				}
+				if (temp.get(index).equals(
+						Config.solutionWord.get(i))) {
 					word = new WordProcessor(temp);
-					//System.out.println(word.getWord());
+					// System.out.println(word.getWord());
 					ImageSearch test = new ImageSearch(word.getWord());
 					urls.add(test.getImageUrl());
-					
-						bigWords.get(randomIndexes.get(j)).setProcessedWord(word.getWord());
-					
-				
-					Config.gameBigWords.add(bigWords.get(randomIndexes.get(j)));
+
+					bigWords.get(randomIndexes.get(j)).setProcessedWord(
+							word.getWord());
+
+					Config.gameBigWords.add(bigWords.get(randomIndexes
+							.get(j)));
 					break;
 				}
 			}
 
 		}
 		for (int i = 0; i < Config.gameBigWords.size(); i++) {
-			System.out.println("Word " + Config.gameBigWords.get(i).getEnglish());
+			System.out.println("Word "
+					+ Config.gameBigWords.get(i).getEnglish());
 		}
 		Config.urls = urls;
 	}
+
 	
-	public void pickSolutionWord(){
+	
+	public void pickSolutionWord() {
 		BigWordCollection possibleSolutions = Config.gameCollectionSolutionBank;
 		Random rand = new Random();
 		BigWord solution;
 		WordProcessor word;
-	
-		
+
 		if (possibleSolutions.size() > 1) {
-			solution = possibleSolutions.getBigWord(rand.nextInt(possibleSolutions.size()-1)+1);
-		}
-		else {
+			solution = possibleSolutions.getBigWord(rand
+					.nextInt(possibleSolutions.size() - 1) + 1);
+		} else {
 			solution = possibleSolutions.getBigWord(0);
 		}
-		
-		if(Config.LANGUAGE.equals("En")){
+
+		if (Config.LANGUAGE.equals("En")) {
 			word = new WordProcessor(solution.getEnglish());
 			Config.solutionWord = word.getLogicalChars();
 			Config.solutionBigWord = solution;
 		} else {
 			word = new WordProcessor(solution.getTelugu());
-			System.out.println("Logical Length: "+ word.getLogicalLength());
+			System.out.println("Logical Length: " + word.getLogicalLength());
 			Config.solutionWord = word.getLogicalChars();
 			Config.solutionBigWord = solution;
-			System.out.println("Size: "+Config.solutionWord.size());
+			System.out.println("Size: " + Config.solutionWord.size());
 		}
-		System.out.println("Solution Word: "+word.getWord());
+		System.out.println("Solution Word: " + word.getWord());
 	}
 }
